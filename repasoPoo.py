2a6fddf461986f2abe3986831a5003b2572fd99e
unknown <paholo2054@gmail.com>
2022-08-17 19:20:32 -0500

    lista de productos

---
+++ repasoPoo.py
@@ -0,0 +1,42 @@
+productos = [
+    {
+        "id": 1,
+        "nombre": "Sandwich de Chocolate", 
+        "precio": 10
+    },
+    {
+        "id": 2,
+        "nombre": "Sandwich de Jamón", 
+        "precio": 10
+    },
+    {
+        "id": 3,
+        "nombre": "Sandwich de Queso", 
+        "precio": 10
+    },
+    {
+        "id": 4,
+        "nombre": "Sandwich de Pollo", 
+        "precio": 10
+    },
+    {
+        "id": 5,
+        "nombre": "Sandwich de Atún", 
+        "precio": 10
+    },
+    {
+        "id": 6,
+        "nombre": "Sandwich de Cebolla", 
+        "precio": 10
+    },
+    {
+        "id": 7,
+        "nombre": "Sandwich de Pescado", 
+        "precio": 10
+    },
+    {
+        "id": 8,
+        "nombre": "Sandwich de Carne",
+        "precio": 10
+    },
+]
