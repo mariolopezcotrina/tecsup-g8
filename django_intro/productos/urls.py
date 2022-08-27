82e40038d3e7bf6ee6c5f77e451717cbc5d6f07c
Paolo <paholo2054@gmail.com>
2022-08-26 23:11:42 -0500

    26/08 Django rest framework

---
+++ django_intro/productos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ProductosView
+
+
+urlpatterns = [
+    path('productos/', ProductosView.as_view())
+]
--- django_intro/productos/views.py
+++ django_intro/productos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Productos
+from .serializers import ProductosSerializer
 
 # Create your views here.
+
+
+class ProductosView(generics.ListAPIView):
+    queryset = Productos.objects.all()
+    serializer_class = ProductosSerializer
+
+    def get(self, request):
+        productos = self.get_serializer(self.get_queryset(), many=True)
+        return Response(data=productos.data, status=status.HTTP_200_OK)
