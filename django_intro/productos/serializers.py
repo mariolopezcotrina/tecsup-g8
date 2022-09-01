f979998acd254d265f321f5e3d9a4e47dda54f41
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-08-31 21:20:12 -0500

    creacion del proyecto de django

--- django_intro/productos/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Productos
-
-
-class ProductosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Productos
-        fields = '__all__'
